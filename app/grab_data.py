0caf821712fb5ae1f93713c9e9e8e521fb4a7565
jakemadison <jakemadison@gmail.com>
2015-09-02 18:11:58 -0700

    poop recommends.

--- app/grab_data.py
+++ app/grab_data.py
@@ -1,10 +1,17 @@
 import requests
+import feedparser
+from random import randint
 
 def get_netflix_data():
 
-    url_root = ''
+    netflix_data = feedparser.parse('http://dvd.netflix.com/Top100RSS')
 
-    return ''
+    choice = randint(0, 99)
+
+    movie_choice = netflix_data['entries'][choice]['title']
+    link = netflix_data['entries'][choice]['link']
+
+    return movie_choice, link
 
 
 if __name__ == "__main__":
--- app/views.py
+++ app/views.py
@@ -7,13 +7,12 @@ import json
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", movie='movie')
+    movie, link = grab_data.get_netflix_data()
+    return render_template("index.html", movie=movie, link=link)
 
 
 @app.route('/get_movie')
-def get_stop(stop=51195):
-    g.route = 0
-    g.stopNumber = stop
-    g.allstops = False
-    return jsonify({'result': 'success', 'movie': 'movie_name'})
+def get_stop():
+    movie, link = grab_data.get_netflix_data()
+    return jsonify({'result': 'success', 'movie': movie})
 
